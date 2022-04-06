eb806dfcb4b3af4f6d9f32ec05460aa75f55e6c8
nazimudheen267 <nazimudheen267@gmail.com>
2022-04-06 11:56:16 +0530

    manifest file added

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".container")
+const coffees = [
+    { name: "Perspiciatis", image: "images/coffee1.jpg" },
+    { name: "Voluptatem", image: "images/coffee2.jpg" },
+    { name: "Explicabo", image: "images/coffee3.jpg" },
+    { name: "Rchitecto", image: "images/coffee4.jpg" },
+    { name: " Beatae", image: "images/coffee5.jpg" },
+    { name: " Vitae", image: "images/coffee6.jpg" },
+    { name: "Inventore", image: "images/coffee7.jpg" },
+    { name: "Veritatis", image: "images/coffee8.jpg" },
+    { name: "Accusantium", image: "images/coffee9.jpg" },];
+
+const showCoffees = () => {
+    let elements = "";
+
+    coffees.forEach((item) => 
+    (elements += `<div class="card">
+    <img class="card--avatar" src=${item.image} />
+    <h1 class="card--title">${item.name}</h1>
+    <a class="card--link" href="#">Taste</a>
+  </div>`))
+
+  container.innerHTML = elements;
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees);
