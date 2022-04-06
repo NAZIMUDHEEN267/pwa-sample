c34c1dfcd15e8718ca634c41668a9dd4c45e5931
nazimudheen267 <nazimudheen267@gmail.com>
2022-04-06 22:47:04 +0530

    prompt checking

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+window.addEventListener("beforeInstallPrompt", (e) => {
+    console.log(e.platforms);
+})
+
 const container = document.querySelector(".container")
+
 const coffees = [
     { name: "Perspiciatis", image: "images/coffee1.jpg" },
     { name: "Voluptatem", image: "images/coffee2.jpg" },
@@ -34,3 +39,29 @@ if('serviceWorker'){
         console.log(e);
     })
 }
+
+const addBtn = document.querySelector(".add-button");
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+  
+    addBtn.addEventListener('click', () => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
+  });
